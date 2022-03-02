06bfeb4f338ce5caf5b4b1df3cd00e086b5d96bf
DanielleAgat <50655428+DanielleAgat@users.noreply.github.com>
2022-03-02 21:47:30 +0200

    Add files via upload

---
+++ LoginViewController.swift
@@ -0,0 +1,21 @@
+//
+//  LoginViewController.swift
+//  SmartBox
+//
+//  Created by agat levi on 02/03/2022.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBOutlet var smartBoxLogo: UILabel!
+    @IBOutlet var userNameTextBox: UITextField!
+    @IBOutlet var passwordTextBox: UITextField!
+}
